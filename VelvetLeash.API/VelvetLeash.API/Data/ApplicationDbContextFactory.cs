14610f8f90c82519a9c311ab067f32dec3dacca1
murtaza2318 <117745682+murtaza2318@users.noreply.github.com>
2025-06-25 17:54:42 -0700

    Add VelvetLeash.API/VelvetLeash.API/Data/ApplicationDbContextFactory.cs 1750899281922

---
+++ VelvetLeash.API/VelvetLeash.API/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace VelvetLeash.API.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            // Using SQLite for development
+            string cs = "Data Source=velvetleash.db";
+            optionsBuilder.UseSqlite(cs);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
